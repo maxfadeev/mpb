5f5e8a12c289ec811c10828a28d4fd4ac0804b9e
masdeft <masdeft@gmail.com>
2015-04-03 17:27:24 +0300

    Add View and Dispatcher services to the Admin module

--- application/modules/admin/Module.php
+++ application/modules/admin/Module.php
@@ -5,10 +5,13 @@ namespace Application\Modules\Admin;
 
 
 use Phalcon\Loader;
+use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\ModuleDefinitionInterface;
+use Phalcon\Mvc\View;
 
 class Module implements ModuleDefinitionInterface
 {
+    protected $di;
 
     /**
      * Registers an autoloader related to the module
@@ -32,6 +35,32 @@ class Module implements ModuleDefinitionInterface
      */
     public function registerServices($di)
     {
-        // TODO: Implement registerServices() method.
+        $this->di = $di;
+        $this->setDispatcher();
+        $this->setView();
+    }
+
+    /**
+     * Sets a Dispatcher service
+     */
+    public function setDispatcher()
+    {
+        $this->di->set('dispatcher', function() {
+            $dispatcher = new Dispatcher();
+            $dispatcher->setDefaultNamespace('Application\Modules\Admin\Controllers');
+            return $dispatcher;
+        });
+    }
+
+    /**
+     * Sets a View service
+     */
+    public function setView()
+    {
+        $this->di->set('view', function() {
+            $view = new View();
+            $view->setViewsDir('../application/modules/admin/views/');
+            return $view;
+        });
     }
 }
