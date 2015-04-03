a7b31256d19fee2f4270514152cd5b2ae2951c87
masdeft <masdeft@gmail.com>
2015-04-03 15:37:05 +0300

    Add application's bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,7 +4,54 @@
 namespace Application;
 
 
+use Phalcon\Config\Adapter\Ini;
+use Phalcon\Mvc\Application;
+
 class Bootstrap
 {
+    /**
+     * @var \Phalcon\DI
+     */
+    protected $di;
+
+    public function __construct()
+    {
+        $this->di = new DiApplication();
+    }
+
+    /**
+     * Creates an Application instance and calls registerModules()
+     *
+     * @return \Phalcon\Mvc\Application
+     */
+    public function createApplication()
+    {
+        $application = new Application($this->di);
+
+        $this->registerModules($application);
+
+        return $application;
+    }
+
+    /**
+     * Registers modules
+     *
+     * @param \Phalcon\Mvc\Application$application
+     */
+    public function registerModules(Application $application)
+    {
+        $application->registerModules($this->getIniConfiguration('modules')->toArray());
+    }
+
+    /**
+     * Gets concrete Ini configuration instance by name
+     *
+     * @param string $name
+     * @return \Phalcon\Config\Adapter\Ini
+     */
+    public function getIniConfiguration($name)
+    {
+        return new Ini("../application/configs/{$name}.ini");
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
 <?php
+
+$loader = new \Phalcon\Loader();
+// register an application namespace
+$loader->registerNamespaces(['Application' => '../application']);
+
+$loader->register();
+
+$bootstrap = new \Application\Bootstrap();
+// create an application
+$application = $bootstrap->createApplication();
+
+// handle a request, then send the response
+echo $application->handle()->getContent();
