7ec670df913bd6354ef5ffd27b4854af319d471e
masdeft <masdeft@gmail.com>
2015-04-10 15:34:37 +0300

    Add User model attributes

--- application/modules/users/models/Users.php
+++ application/modules/users/models/Users.php
@@ -8,5 +8,43 @@ use Phalcon\Mvc\Model;
 
 class Users extends Model
 {
+    /**
+     * @var integer
+     */
+    public $id;
 
+    /**
+     * @var string
+     */
+    public $login;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @var string
+     */
+    public $role;
+
+    /**
+     * @var boolean
+     */
+    public $banned;
+
+    /**
+     * @var boolean
+     */
+    public $suspended;
+
+    /**
+     * @var boolean
+     */
+    public $active;
 } 
