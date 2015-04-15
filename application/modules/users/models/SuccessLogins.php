6d6f389be22b027675228bb17ce0749854c37f25
masdeft <masdeft@gmail.com>
2015-04-15 10:43:03 +0300

    Add a SuccessLogins model

---
+++ application/modules/users/models/SuccessLogins.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Application\Modules\Users\Models;
+
+
+use Phalcon\Mvc\Model;
+
+class SuccessLogins extends Model
+{
+    /**
+     * @var integer
+     */
+    public $uid;
+
+    /**
+     * @var string
+     */
+    public $ip;
+
+    /**
+     * @var string
+     */
+    public $userAgent;
+}
