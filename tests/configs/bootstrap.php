7a7dc23f872ec50032e7f88c00cb84d3e0caf8c5
masdeft <masdeft@gmail.com>
2015-05-12 11:33:17 +0300

    Add a phalcon incubator to vendors

--- tests/configs/bootstrap.php
+++ tests/configs/bootstrap.php
@@ -3,6 +3,8 @@
 define('BASE_DIR', dirname(dirname(__DIR__)));
 define('APP_DIR', BASE_DIR . '/application');
 
+require BASE_DIR . '/vendor/autoload.php';
+
 $loader = new \Phalcon\Loader();
 // register an application namespace
 $loader->registerNamespaces(['Application' => BASE_DIR. '/application']);
