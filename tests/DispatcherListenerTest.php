ea1fbee34c378349c83112327e73bcbf25ac1e50
masdeft <masdeft@gmail.com>
2015-05-12 17:31:05 +0300

    Add a test DispatcherListenerTest

---
+++ tests/DispatcherListenerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Tests;
+
+
+use Phalcon\Config;
+use Phalcon\DiInterface;
+use Phalcon\Test\UnitTestCase;
+
+class DispatcherListenerTest extends UnitTestCase
+{
+    /**
+     * @var \Application\Acl
+     */
+    protected $acl;
+
+    /**
+     * @var \Application\Modules\Users\Components\Auth
+     */
+    protected $auth;
+
+    public function setUp(DiInterface $di = null, Config $config = null)
+    {
+        parent::setUp($di, $config);
+
+        $this->acl = $this->getMockBuilder('Application\Acl')
+            ->setMethods(['isPrivate', 'isAllowed'])
+            ->getMock();
+
+        $this->acl->expects($this->any())
+            ->method('isPrivate')
+            ->will($this->returnValue(true));
+        $this->acl->expects($this->any())
+            ->method('isAllowed')
+            ->will($this->returnValue(true));
+
+        $this->auth = $this->getMockBuilder('Application\Modules\Users\Components\Auth')
+            ->setMethods(['getIdentity'])
+            ->getMock();
+
+        $this->auth->expects($this->any())
+            ->method('getIdentity')
+            ->will($this->returnValue(['id' => 7, 'login' => 'some-login']));
+    }
+
+    public function testBeforeExecuteRoute()
+    {
+        $this->assertTrue($this->acl->isPrivate('users'));
+
+        $this->assertTrue($this->acl->isAllowed('some-login', 'users', 'add'));
+    }
+}
