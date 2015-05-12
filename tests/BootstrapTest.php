bc2d732636dad8148c00c1e7dae36c10d6087709
masdeft <masdeft@gmail.com>
2015-05-12 14:27:48 +0300

    BootstrapTest a bit refactoring

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -5,15 +5,27 @@ namespace Tests;
 
 
 use Application\Bootstrap;
+use Phalcon\Config;
 use Phalcon\Config\Adapter\Ini;
+use Phalcon\DiInterface;
+use Phalcon\Test\UnitTestCase;
 
-class BootstrapTest extends \PHPUnit_Framework_TestCase
+class BootstrapTest extends UnitTestCase
 {
+    /**
+     * @var \Application\Bootstrap
+     */
     protected $bootstrap;
 
-    public function setUp()
+    public function setUp(DiInterface $di = null, Config $config = null)
     {
+        parent::setUp($di, $config);
         $this->bootstrap = new Bootstrap();
+    }
+
+    public function testClassHasDiAttribute()
+    {
+        $this->assertClassHasAttribute('di', get_class($this->bootstrap));
     }
 
     public function testSetDb()
@@ -22,11 +34,14 @@ class BootstrapTest extends \PHPUnit_Framework_TestCase
 
         $config = new Ini(APP_DIR . "/configs/application.ini");
 
-        $this->assertClassHasAttribute('di', get_class($this->bootstrap));
-
         $this->assertObjectHasAttribute('db', $config);
         $this->assertObjectHasAttribute('host', $config->db);
         $this->assertObjectHasAttribute('username', $config->db);
         $this->assertObjectHasAttribute('password', $config->db);
+    }
+
+    public function testSetRotes()
+    {
+        $this->assertFileExists(APP_DIR . '/configs/routes.ini');
     }
 }
