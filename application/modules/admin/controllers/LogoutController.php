5e000d302abf6526244c824470cc3bdb213b27fc
masdeft <masdeft@gmail.com>
2015-05-13 15:39:17 +0300

    Refactor a bit the logout controller

--- application/modules/admin/controllers/LogoutController.php
+++ application/modules/admin/controllers/LogoutController.php
@@ -6,8 +6,14 @@ namespace Application\Modules\Admin\Controllers;
 
 use Phalcon\Mvc\Controller;
 
+/**
+ * @property \Application\Modules\Users\Components\Auth auth
+ */
 class LogoutController extends Controller
 {
+    /**
+     * Logs out the user
+     */
     public function indexAction()
     {
         $this->auth->remove();
