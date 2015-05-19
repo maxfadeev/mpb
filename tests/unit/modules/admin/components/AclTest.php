38829fa3c500f2f9ce74afb4fdad13cbf2426b2f
masdeft <masdeft@gmail.com>
2015-05-19 14:47:32 +0300

    Test the acl rebuild

--- tests/unit/modules/admin/components/AclTest.php
+++ tests/unit/modules/admin/components/AclTest.php
@@ -4,22 +4,34 @@
 namespace Tests\Unit\Modules\Admin\Components;
 
 
+use Application\Modules\Admin\Components\Acl;
 use Phalcon\Config;
-use Phalcon\DiInterface;
-use Phalcon\Test\UnitTestCase;
 
-class AclTest extends UnitTestCase
+class AclTest extends \PHPUnit_Framework_TestCase
 {
     protected $acl;
 
-    public function setUp(DiInterface $di = null, Config $config = null)
+    public function setUp()
     {
-        parent::setUp($di, $config);
         $this->acl = $this->getMockBuilder('Application\Modules\Admin\Components\Acl')->getMock();
     }
 
     public function testPrivateResources()
     {
         $this->assertClassHasAttribute('privateResources', get_class($this->acl));
+    }
+
+    public function testRebuild()
+    {
+        $aclMemoryAdapter = (new Acl())->rebuild();
+
+        $this->assertTrue($aclMemoryAdapter->getDefaultAction() === \Phalcon\Acl::DENY);
+
+        $this->assertNotEmpty($aclMemoryAdapter->getRoles());
+        $this->assertNotEmpty($aclMemoryAdapter->getResources());
+
+        $this->assertTrue($aclMemoryAdapter->isAllowed('admin', 'index', 'index'));
+
+        $this->assertObjectHasAttribute('filePath', $this->acl);
     }
 }
