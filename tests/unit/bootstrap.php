677f2a3323a2c1af1defb299c97ccbdbe8ef871b
masdeft <masdeft@gmail.com>
2015-06-09 11:46:33 +0300

    Update TravisCI configuration file

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -3,7 +3,7 @@
 define('BASE_DIR', dirname(dirname(__DIR__)));
 define('APP_DIR', BASE_DIR . '/application');
 
-require BASE_DIR . '/vendor/autoload.php';
+//require BASE_DIR . '/vendor/autoload.php';
 
 $loader = new \Phalcon\Loader();
 // register an application namespace
