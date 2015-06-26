99fe97777cb32d7f0fe215d09540fb3787e65f2a
masdeft <masdeft@gmail.com>
2015-06-26 15:45:42 +0300

    Setup travis ci

--- tests/unit/ci/setup_db.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-DIR=$(readlink -enq $(dirname $0))
-
-mysql -uroot -e 'create database blog charset=utf8 collate=utf8_unicode_ci;' && mysql -uroot blog < "$DIR/../../data/blog.sql"
-wait
