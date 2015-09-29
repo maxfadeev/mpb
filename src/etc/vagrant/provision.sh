16b189225d720dffa832a069bd44d604bde16573
max.fadeev <masdeft@gmail.com>
2015-09-29 23:52:22 +0300

    Fix provision.sh

--- src/etc/vagrant/provision.sh
+++ src/etc/vagrant/provision.sh
@@ -76,7 +76,6 @@ slowlog = /var/log/php-fpm/www-slow.log
 php_admin_value[error_log] = /var/log/php-fpm/www-error.log
 php_admin_flag[log_errors] = on
 php_value[session.save_handler] = files
-php_value[session.save_path]    = /var/lib/php/session
 php_value[soap.wsdl_cache_dir]  = /var/lib/php/wsdlcache
 EOF
 
